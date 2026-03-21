95b9720e738d8a5d47512fe96c25516511fc83b1
Max Geißinger <maxgeissinger@gmail.com>
2026-03-21 11:07:27 +0100

    fix: KIZusammenfassung icon in scanner primary color

--- components/KIZusammenfassung.tsx
+++ components/KIZusammenfassung.tsx
@@ -11,7 +11,7 @@ interface Props {
 function ShieldIcon() {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" stroke="#8B5CF6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" stroke="#FF2D55" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
